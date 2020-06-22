5f2db817dad4e50699b0bf5a357ac39f3ddaf18e
Avraham Kirsch <etrog95@gmail.com>
2020-06-22 15:51:20 +0300

    Initial Commit

---
+++ webView-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webView-test
+//
+//  Created by admin on 22/06/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
